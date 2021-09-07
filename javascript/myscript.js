128f956297fe57075c048ae7a4ae45dafa295c56
DavideZattra <d.zattra@gmail.com>
2021-09-07 15:35:17 +0200

    point 3 and point 4 done

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -8,14 +8,44 @@
  * 
  */
 
-var studentOne = { //Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età.
+const studentOne = { //Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età.
     name : 'Davide',
     surname : 'Zattra',
     age : 27
 }
 
-for(let data in studentOne){ // Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
+for(let properties in studentOne){ // Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
 
-    console.log(studentOne[data])
+    console.log(studentOne[properties])
+
+}
+
+const studentsList = { //Creare un array di oggetti di studenti.
+
+    student : [
+        {
+            name : 'Davide',
+            surname : 'Zattra',
+            age : 27
+        },
+
+        {
+            name : 'Eugenio',
+            surname : 'Tacconi',
+            age : 75
+        },
+
+        {
+            name : 'Mario',
+            surname : 'Rossi',
+            age: 25
+        }
+    ]
+}
+
+for (let i in studentsList.student) { //Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
+
+    console.log(studentsList.student[i].name)
+    console.log(studentsList.student[i].surname)
 
 }
